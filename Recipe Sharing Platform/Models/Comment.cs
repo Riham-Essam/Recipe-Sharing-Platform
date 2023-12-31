b5095fd17c3584275cd3538d9f67a37deaffcff4
Riham Essam <rihamessam1234@gmail.com>
2023-12-31 03:33:23 +0200

    Add Recipe, AppDbContext and Comment models

---
+++ Recipe Sharing Platform/Models/Comment.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Recipe_Sharing_Platform.Models
+{
+    public class Comment
+    {
+    }
+}
---
+++ Recipe Sharing Platform/Models/Recipe.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Recipe_Sharing_Platform.Models
+{
+    public class Recipe
+    {
+        [Required]
+        public string RecipeId { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Description { get; set; }
+        public string Photo { get; set; }
+        [Required]
+        public string Ingredients { get; set; }
+        public string Steps { get; set; }
+
+    }
+}
