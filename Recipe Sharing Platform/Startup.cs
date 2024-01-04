dcea29caae78620e630edb6f62b0a7a6733de7cf
Riham Essam <rihamessam1234@gmail.com>
2024-01-04 16:10:07 +0200

    Update Startup.cs

--- Recipe Sharing Platform/Startup.cs
+++ Recipe Sharing Platform/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -26,6 +27,13 @@ namespace Recipe_Sharing_Platform
         {
             services.AddDbContextPool<AppDbContext>(options =>
             options.UseSqlServer(config.GetConnectionString("DefaultConnection")));
+
+            services.AddMvc();
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<AppDbContext>()
+                .AddDefaultTokenProviders();
+;
+            services.AddScoped<IRecipeRepository,SQLRecipeRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
