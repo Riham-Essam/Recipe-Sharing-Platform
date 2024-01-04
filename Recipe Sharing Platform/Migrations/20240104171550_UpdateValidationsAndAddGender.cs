b512c843c8179a39fac61f6278ca9ff26bb44ced
Riham Essam <rihamessam1234@gmail.com>
2024-01-04 19:21:07 +0200

    Update Migration

---
+++ Recipe Sharing Platform/Migrations/20240104171550_UpdateValidationsAndAddGender.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Recipe_Sharing_Platform.Migrations
+{
+    public partial class UpdateValidationsAndAddGender : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "AspNetUsers",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "AspNetUsers",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AddColumn<int>(
+                name: "Gender",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Gender",
+                table: "AspNetUsers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "AspNetUsers",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 30);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "AspNetUsers",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 30);
+        }
+    }
+}
