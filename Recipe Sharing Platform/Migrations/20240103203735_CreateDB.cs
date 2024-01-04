a644f38c34996e950cdbb7476771762dd312e8dd
Riham Essam <rihamessam1234@gmail.com>
2024-01-04 16:05:52 +0200

    Update Migration

---
+++ Recipe Sharing Platform/Migrations/20240103203735_CreateDB.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Recipe_Sharing_Platform.Migrations
+{
+    public partial class CreateDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "RecipeId",
+                table: "Comments",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "RecipeId",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_RecipeId",
+                table: "Comments",
+                column: "RecipeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_RecipeId",
+                table: "AspNetUsers",
+                column: "RecipeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_Recipes_RecipeId",
+                table: "AspNetUsers",
+                column: "RecipeId",
+                principalTable: "Recipes",
+                principalColumn: "RecipeId",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_Recipes_RecipeId",
+                table: "Comments",
+                column: "RecipeId",
+                principalTable: "Recipes",
+                principalColumn: "RecipeId",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_Recipes_RecipeId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_Recipes_RecipeId",
+                table: "Comments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Comments_RecipeId",
+                table: "Comments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_RecipeId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "RecipeId",
+                table: "AspNetUsers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "RecipeId",
+                table: "Comments",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
