474a961bfaae2ba3c4b92737fb5e6f75f993cf61
Riham Essam <rihamessam1234@gmail.com>
2024-01-04 16:08:01 +0200

    Add home and account controllers

---
+++ Recipe Sharing Platform/Controllers/AccountController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Recipe_Sharing_Platform.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Recipe_Sharing_Platform.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly SignInManager<ApplicationUser> signInManager;
+
+        public AccountController(UserManager<ApplicationUser> userManager,
+                                 SignInManager<ApplicationUser> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+
+       // [HttpGet]
+
+    }
+}
---
+++ Recipe Sharing Platform/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Recipe_Sharing_Platform.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Recipe_Sharing_Platform.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IRecipeRepository recipeRepository;
+
+        public HomeController(IRecipeRepository recipeRepository)
+        {
+            this.recipeRepository = recipeRepository;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Recipe> recipe = recipeRepository.GetAllRecipes();
+
+            return View(recipe);
+        }
+
+    }
+}
