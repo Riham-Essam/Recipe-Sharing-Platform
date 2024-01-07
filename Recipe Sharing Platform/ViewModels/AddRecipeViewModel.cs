c0416e31be188f2131a19a4065d3249e22eaade8
Riham Essam <rihamessam1234@gmail.com>
2024-01-07 12:00:02 +0200

    Create AddRecipeViewModel.cs

---
+++ Recipe Sharing Platform/ViewModels/AddRecipeViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Recipe_Sharing_Platform.ViewModels
+{
+    public class AddRecipeViewModel
+    {
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Description { get; set; }
+        public IFormFile Image { get; set; }
+        [Required]
+        public string Ingredients { get; set; }
+        public string Steps { get; set; }
+    }
+}
