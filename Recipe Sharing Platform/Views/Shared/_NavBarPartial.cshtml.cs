44616d491b84d2ce4afc1356da49ef7cb3170ab7
Riham Essam <rihamessam1234@gmail.com>
2024-01-10 20:55:13 +0200

    Add partialView for vertical nav bar

---
+++ Recipe Sharing Platform/Views/Shared/_NavBarPartial.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Recipe_Sharing_Platform.Views.Shared
+{
+    public class _NavBarPartialModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
