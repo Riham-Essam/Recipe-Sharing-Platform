7a359b858526ad340f6c6ba8a7ca814fb89b989d
Riham Essam <rihamessam1234@gmail.com>
2024-01-10 20:57:37 +0200

    Create toggleInput.js

---
+++ Recipe Sharing Platform/wwwroot/js/toggleInput.js
@@ -0,0 +1,12 @@
+﻿function toggleInput(id) {
+    var containerElement = document.getElementById(id);
+
+    if (containerElement.style.display === 'none') {
+
+        containerElement.style.display = 'block';
+
+    } else {
+
+        containerElement.style.display = 'none';
+    }
+}
